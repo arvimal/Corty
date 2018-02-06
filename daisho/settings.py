6832a4214c1fcea2373316cd93b6b9dbaa36b043
Vimal A.R <arvimal@yahoo.in>
2018-02-06 23:53:52 +0530

    * Moving settings/basic_checks to settings.py, not yet finished.

--- daisho/settings.py
+++ daisho/settings.py
@@ -1,19 +1,62 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2016 Vimal A.R <arvimal@yahoo.in>
+import pathlib
+import os
+import daisho_db
 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+HOME = os.getenv('HOME')
+DAISHO_HOME = HOME + "/.config/daisho/"
+CONFIG = DAISHO_HOME + "daisho.conf"
+HISTORY = DAISHO_HOME + "history.txt"
+LOG_FILE = DAISHO_HOME + "daisho.log"
 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+class Daisho(object):
+    """Daisho's main class"""
+
+    def __init__(self):
+        # Check existence of CONFIG
+        # Move logging to its own file
+        if all([pathlib.Path(CONFIG).exists()]):
+
+            print("\n\t- Welcome to Daisho -\n")
+            # Check if we are able to connect to MongoDB.
+            daisho_db.mongo_conn()
+            self.daisho_help()
+            self.daisho_prompt()
+            logging.info("Started Daisho prompt.")
+
+        else:
+            print("\n\t- Welcome to Daisho -\n")
+            print("Initial setup:")
+            print("\tCreating Daisho's configurations")
+
+            # Create HOME, CONFIG, HISTORY, and LOG_FILE
+            pathlib.Path(DAISHO_HOME).mkdir()
+            pathlib.Path(CONFIG).touch(exist_ok=True)
+            pathlib.Path(HISTORY).touch(exist_ok=True)
+            pathlib.Path(LOG_FILE).touch(exist_ok=True)
+            # Write Daisho's configuration file
+            conf_parser = configparser.ConfigParser()
+            conf_parser.add_section("Global")
+            conf_parser.set("Global", "DAISHO_HOME", DAISHO_HOME)
+            conf_parser.set("Global", "CONFIG", CONFIG)
+            conf_parser.set("Global", "HISTORY", HISTORY)
+            conf_parser.set("Global", "LOG_FILE", LOG_FILE)
+            with open(CONFIG, "w") as config_file:
+                conf_parser.write(config_file)
+            print("\tDone")
+
+            # Configure logging from here
+            logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
+            logging.info("Generating configuration files.")
+            logging.info("#### Daisho starting up ####")
+            # Check if we are able to connect to MongoDB.
+            daisho_db.mongo_conn()
+            self.daisho_help()
+            self.daisho_prompt()
+            logging.info("Started Daisho prompt.")
+
 
 
