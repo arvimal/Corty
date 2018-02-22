c699191b30044f2388ec8f6c79261db811c4a32e
Vimal A.R <arvimal@yahoo.in>
2018-02-22 20:03:59 +0530

    * Modified daisho/daisho.py

--- daisho/daisho.py
+++ daisho/daisho.py
@@ -26,6 +26,7 @@ if sys.version[0] != "3":
 import os
 import pathlib
 import configparser
+import datetime
 from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
@@ -33,6 +34,7 @@ from prompt_toolkit.contrib.completers import WordCompleter
 import daisho_db
 import daisho_logger
 import daisho_add
+import settings
 
 HOME = os.getenv('HOME')
 DAISHO_HOME = HOME + "/.config/daisho/"
@@ -41,52 +43,7 @@ HISTORY = DAISHO_HOME + "history.txt"
 LOG_FILE = DAISHO_HOME + "daisho.log"
 
 
-class Daisho(object):
-    """Daisho's main class"""
-
-    def __init__(self):
-        # Check existence of CONFIG
-        # Move logging to its own file
-        if all([pathlib.Path(CONFIG).exists()]):
-
-            print("\n\t- Welcome to Daisho -\n")
-            # Check if we are able to connect to MongoDB.
-            daisho_db.mongo_conn()
-            self.daisho_help()
-            self.daisho_prompt()
-            logging.info("Started Daisho prompt.")
-
-        else:
-            print("\n\t- Welcome to Daisho -\n")
-            print("Initial setup:")
-            print("\tCreating Daisho's configurations")
-
-            # Create HOME, CONFIG, HISTORY, and LOG_FILE
-            pathlib.Path(DAISHO_HOME).mkdir()
-            pathlib.Path(CONFIG).touch(exist_ok=True)
-            pathlib.Path(HISTORY).touch(exist_ok=True)
-            pathlib.Path(LOG_FILE).touch(exist_ok=True)
-            # Write Daisho's configuration file
-            conf_parser = configparser.ConfigParser()
-            conf_parser.add_section("Global")
-            conf_parser.set("Global", "DAISHO_HOME", DAISHO_HOME)
-            conf_parser.set("Global", "CONFIG", CONFIG)
-            conf_parser.set("Global", "HISTORY", HISTORY)
-            conf_parser.set("Global", "LOG_FILE", LOG_FILE)
-            with open(CONFIG, "w") as config_file:
-                conf_parser.write(config_file)
-            print("\tDone")
-
-            # Configure logging from here
-            logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
-            logging.info("Generating configuration files.")
-            logging.info("#### Daisho starting up ####")
-            # Check if we are able to connect to MongoDB.
-            daisho_db.mongo_conn()
-            self.daisho_help()
-            self.daisho_prompt()
-            logging.info("Started Daisho prompt.")
-
+class Daisho:
     def daisho_help(self):
         """
         Daisho's Usage
