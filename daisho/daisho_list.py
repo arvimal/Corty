be35a12849c7943aa80c8d7cebc244173da2ed9b
Vimal A.R <arvimal@yahoo.in>
2018-03-14 23:23:19 +0530

    * Added daisho/daisho_list.py
    	* This takes care of listing tasks and notes based on various criteria.
    
    	* This also handles the editing functions for existing notes/tasks.

---
+++ daisho/daisho_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2016 Vimal A.R <arvimal@yahoo.in>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+"""
+* This module deals with listing the tasks and notes, 
+based on various criteria.
+
+* This also takes care on opening tasks which has active sub-tasks, 
+as well as notes in the pre-configured editor of your choice.
+"""
+
