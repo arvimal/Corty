05cecc4483fc451d955e0d6f7cbaf348a02251e7
Vimal A R <arvimal81@gmail.com>
2021-09-04 18:27:08 +0200

    Client

--- daisho/client/daisho_add.py
+++ daisho/client/daisho_add.py
@@ -36,6 +36,8 @@ from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.history import FileHistory
 
+from server import daisho_db
+
 logger = logging.getLogger(__name__)
 HOME = os.getenv("HOME")
 DAISHO_HOME = HOME + "/.config/daisho/"
